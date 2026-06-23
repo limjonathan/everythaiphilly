f691e8deb45a4e252b1cf2ed0adde587e83900d6
Jonathan Lim <jonathanlim@Jonathans-MacBook-Pro.local>
2026-06-23 02:07:04 -0400

    Remove Vite build setup to use pure HTML5 and Vanilla CSS

--- vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './', // Generates relative paths for all assets to support deployment on subpaths (like GitHub Pages)
-});
